f5aa3cd3f19ba40fd261c9d6ba63d9b74f6ca5fa
Duane M. Moody <dmmoody@gmail.com>
2015-02-13 10:07:30 -0800

    add evaluation for equilateral

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 var triangles = function(side1, side2, side3) {
-	if ((side1 + side2 >= side3) || (side2 + side3 >= side1) || (side3 + side1 >= side2)) {
+	if ((side1 + side2 <= side3) || (side2 + side3 <= side1) || (side3 + side1 <= side2)) {
 		return false;
+	} else if ((side1 === side2) && (side2 === side3)) {
+		return 'equilateral';
+	} else {
+		return true;
 	}
 };
