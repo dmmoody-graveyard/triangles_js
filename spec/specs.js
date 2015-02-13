3b68896bacfd7cc33c8213c34a758c84c80f1b0a
Duane M. Moody <dmmoody@gmail.com>
2015-02-13 09:52:37 -0800

    add spec to evaluate equilateral

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('triangles', function() {
 	it('takes three line lengths and returns false for not being a triangle', function() {
 		expect(triangles(3, 5, 10)).to.equal(false);
 	});
+
+	it('takes three line lengths and returns equilateral', function() {
+		expect(triangles(3, 3, 3)).to.equal('equilateral');
+	});
 });
