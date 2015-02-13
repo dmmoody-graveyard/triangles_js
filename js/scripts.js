5153990ef924aa2e0e3dfa12246b961554f0ef1f
Duane M. Moody <dmmoody@gmail.com>
2015-02-13 10:36:10 -0800

    add document ready and retrieve input from form

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,17 @@ var triangles = function(side1, side2, side3) {
 		return 'scalene';
 	}
 };
+
+$(document).ready(function() {
+	$('form#sides').submit(function(event) {
+		var side1 = parseInt($('input#side1').val());
+		var side2 = parseInt($('input#side2').val());
+		var side3 = parseInt($('input#side3').val());
+		var result = triangles(side1, side2, side3);
+
+		$('.result').text(result);
+
+		$('#result').show()
+		event.preventDefault();
+	});
+});
